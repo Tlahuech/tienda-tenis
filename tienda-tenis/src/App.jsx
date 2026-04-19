a29ed62e78bd0df87796397972eaeecff0cb9b91
Tlahuech <tlahuechedgar@gmail.com>
2026-04-19 12:35:57 -0600

    Primera modificacion

--- tienda-tenis/src/App.jsx
+++ tienda-tenis/src/App.jsx
@@ -4,118 +4,53 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+  function App() {
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+    <div>
+      <h1>Mi tienda de tenis 👟</h1>
+      <h3>Se agrega la modificacion agrega la modificacion</h3>
+      <Modulo1 />
+         <Modulo2 />
+    </div>
+    
+  );
+}
+// Modulo1.jsx
+function Modulo1() {
+  return (
+    <section>
+      <h2>Módulo 1</h2>
+      <Orden1 />
+    </section>
+  )
+}
+ 
+function Modulo2(){
+  return (
+    <div style={{ border: '1px solid blue', margin: '10px', padding: '10px' }}>
+      <h2>Módulo 2</h2>
+      <Orden posicion="8" />
+    </div>
   )
 }
 
+function Orden({ posicion }) {
+  return (
+    <div>
+      <h3>Orden {posicion === '6' ? '1' : '2'}</h3>
+      <p>Posición {posicion}</p>
+    </div>
+  )
+}
+
+// Orden1.jsx  
+function Orden1() {
+  return (
+    <div>
+      <h3>Orden 1</h3>
+      <p>Posición 6</p>
+    </div>
+  )
+}
 export default App
